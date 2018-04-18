8cf532c139e7fb653799ca96b6f650ca03155a92
kimheegeon <32567051+kimheegeon@users.noreply.github.com>
2018-04-18 10:28:43 +0900

    MAIN TEST

--- src/view/SeatView.java
+++ src/view/SeatView.java
@@ -1,5 +1,37 @@
 package view;
 
-public class SeatView {
+import java.awt.Panel;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
 
+import java.awt.Color;
+
+public class SeatView extends Panel{
+	public SeatView() {
+		setBackground(Color.WHITE);
+		setLayout(null);
+		
+		JLabel jl_Screen = new JLabel("SCREEN");
+		jl_Screen.setOpaque(true);
+		jl_Screen.setForeground(Color.black);
+		jl_Screen.setBackground(Color.LIGHT_GRAY);
+		jl_Screen.setBounds(27, 58, 560, 35);
+		jl_Screen.setVerticalAlignment(SwingConstants.CENTER);
+		jl_Screen.setHorizontalAlignment(SwingConstants.CENTER);
+		add(jl_Screen);
+		
+		JLabel[][] seat = new JLabel[8][10];
+		
+		for(int i = 0 ; i<8 ; i ++)
+			for(int j = 0 ; j < 10 ; j++){
+				char row = (char)('A'+i);
+				String col =String.valueOf((j+1));
+				seat[i][j].setText(row+col);
+				if(j>5)								//27,45 가 시작점
+					seat[i][j].setBounds(27+i,); 
+				add(seat[i][j]);
+				
+			}
+		
+	}
 }
--- src/view/TheaterMain.java
+++ src/view/TheaterMain.java
@@ -1,5 +1,24 @@
 package view;
 
-public class TheaterMain {
+import javax.swing.JFrame;
+
+public class TheaterMain extends JFrame{
+	public TheaterMain() {
+		
+		
+		SeatView sv = new SeatView();
+		
+		this.add(sv);
+		setSize(800,600);
+		setVisible(true);
+		
+	}
+	
+	public static void main(String[] args) {
+		TheaterMain tm = new TheaterMain();
+		
+		
+		
+	}
 
 }
