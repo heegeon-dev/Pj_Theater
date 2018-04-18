9d10f193ea88fb7858ffe8ffa84f1b3b09cf7210
student <student@M15014>
2018-04-18 11:25:01 +0900

    startview

--- src/view/StartView.java
+++ src/view/StartView.java
@@ -2,57 +2,33 @@ package view;
 
 import java.awt.Color;
 import java.awt.EventQueue;
-import java.awt.Font;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
+
 import javax.swing.ImageIcon;
-import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 
-public class StartView extends JFrame implements ActionListener{
+public class StartView extends JPanel {
 
-	private JPanel contentPane;
 	private JLabel lbBookingPrint, lbStartView;
 	private JLabel lbTicketBuy;
+	JPanel contentPane;
 	
-
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					StartView frame = new StartView();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
 
 	public StartView() {
 		addLayout();
 		eventProc();
 	}
-	
-	
+
 	void eventProc(){
 		
 		MyMouseListener mc = new MyMouseListener();
 		lbBookingPrint.addMouseListener( mc );
 		lbTicketBuy.addMouseListener( mc );
-//		lblNewLabel_1.addMouseListener( new MouseAdapter() { 
-//			public void mouseClicked(MouseEvent e) {
-//						JOptionPane.showMessageDialog(null, "다음으로 넘어갑니다.");
-//			}
-//		});
-//		
 	}
 	
 	class MyMouseListener extends MouseAdapter{
@@ -65,23 +41,17 @@ public class StartView extends JFrame implements ActionListener{
 			}else if( evt == lbBookingPrint ){
 				JOptionPane.showMessageDialog(null, "2번 다음으로 넘어갑니다.");
 			}
-		}	// inner class
-	}
-	@Override
-	public void actionPerformed(ActionEvent e) {
-			
-		
-	}
-	
+		}
+	}	// inner class
 	
 	public void addLayout(){
-		setResizable(false);
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	
 		setBounds(100, 100, 800, 600);
 		contentPane = new JPanel();
+		contentPane.setBounds(100, 100, 800, 600);
 		contentPane.setBackground(Color.WHITE);
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		setContentPane(contentPane);
+		
 		contentPane.setLayout(null);
 		
 		lbStartView = new JLabel("");
@@ -97,6 +67,4 @@ public class StartView extends JFrame implements ActionListener{
 		lbTicketBuy.setBounds(77, 280, 204, 153);
 		contentPane.add(lbTicketBuy);
 	}
-	
-	
 }
--- src/view/TheaterMain.java
+++ src/view/TheaterMain.java
@@ -2,23 +2,23 @@ package view;
 
 import javax.swing.JFrame;
 
+
 public class TheaterMain extends JFrame{
 	public TheaterMain() {
 		
 		
-		SeatView sv = new SeatView();
+		StartView sv2 = new StartView();
 		
-		this.add(sv);
-		setSize(800,600);
+		add(sv2.contentPane);
+		setSize(800, 600);
 		setVisible(true);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
 	}
 	
 	public static void main(String[] args) {
 		TheaterMain tm = new TheaterMain();
-		
-		
-		
+			
 	}
-
+	
 }
