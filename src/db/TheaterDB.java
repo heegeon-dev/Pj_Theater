f89aad0e146cb99ea67fdc46f63804bf6eba9d62
kimheegeon <32567051+kimheegeon@users.noreply.github.com>
2018-04-19 10:41:22 +0900

    되라 db

---
+++ src/db/TheaterDB.java
@@ -0,0 +1,22 @@
+package db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class TheaterDB {
+	static Connection con;
+	String driver = "oracle.jdbc.driver.OracleDriver";
+	String url = "jdbc:oracle:thin:@70.12.115.56:1521:orcl";
+	String user = "heegeon";
+	String passw = "1234";
+
+	private TheaterDB() throws Exception {
+		getClass().forName(driver);
+		con= DriverManager.getConnection(url,user,passw);
+	}
+	
+	public static Connection getConnection() throws Exception{
+		if(con == null) new TheaterDB();
+		return con;
+	}
+}
