0b121d7d831dd5b0a34d0a258958b68d9946ac76
Eun Ji <yikitaeunjee@naver.com>
2018-04-24 17:07:17 +0900

    출력화면 수정

--- src/model/BookingModel.java
+++ src/model/BookingModel.java
@@ -4,7 +4,10 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Locale;
 
 import db.TheaterDB;
 
@@ -33,20 +36,38 @@ public class BookingModel {
 	public ArrayList<String> getinfoByTel(String tel) {
 
 		try {
-			String sql = " SELECT m.TITLE title, b.MOVIEDATE moviedate, b.RUNTIME runtime, b.SEAT seat, p.SUMOF sumof, p.OPTIONOF optionof "
-					+ " FROM movie m , Booking b , payment p "
-					+ " where m.MOVIE_NO = b.MOVIE_NO and b.PAYNUM = p.PAYNUM " + " and b.tel=" + tel;
+			SimpleDateFormat format3 = new SimpleDateFormat("HH:mm", Locale.KOREA);
+			Calendar c = Calendar.getInstance();
+
+			String sql = " select m.TITLE TITLE, s.MOVIEDATE MOVIEDATE, "
+					+ " b.RUNTIME RUNTIME, b.SEAT SEAT, p.SUMOF SUMOF, p.OPTIONOF OPTIONOF "
+					+ " from movie m, booking b, screen s, payment p "
+					+ " where m.movie_no = b.movie_no and b.screenid = s.screenid and " + " b.paynum = p.paynum "
+					+ " and tel = ?";
 			PreparedStatement ps = con.prepareStatement(sql);
+			ps.setString(1, tel);
 
 			ResultSet rs = ps.executeQuery();
 
 			rs.next();
-			infoList.add(rs.getString("title"));
-			infoList.add(rs.getString("moviedate"));
-			infoList.add(rs.getString("runtime"));
-			infoList.add(rs.getString("seat"));
-			infoList.add(rs.getString("sumof"));
-			infoList.add(rs.getString("optionof"));
+			infoList.add(rs.getString("TITLE"));
+			infoList.add(rs.getString("MOVIEDATE"));
+			String start = rs.getString("RUNTIME");
+
+			System.out.println(start);// 21:30
+			System.out.println();
+			format3.parse(start);
+			c.setTime(format3.parse(start));
+			c.add(Calendar.HOUR_OF_DAY, 2);
+			String end = format3.format(c.getTime());
+			
+			String runtime = start +" ~ " + end;
+			infoList.add(runtime);
+			//infoList.add(rs.getString("RUNTIME"));
+
+			infoList.add(rs.getString("SEAT"));
+			infoList.add(rs.getString("SUMOF"));
+			infoList.add(rs.getString("OPTIONOF"));
 
 		} catch (Exception e) {
 			System.out.println("예매내역 출력 실패" + e.getMessage());
